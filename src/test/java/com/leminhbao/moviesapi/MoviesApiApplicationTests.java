92aaeae43277eb802b0e0235fad48bdb18a725d7
Lê Minh Bảo <besilent308@gmail.com>
2024-02-20 22:04:23 +0700

    Initial commit

---
+++ src/test/java/com/leminhbao/moviesapi/MoviesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leminhbao.moviesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
