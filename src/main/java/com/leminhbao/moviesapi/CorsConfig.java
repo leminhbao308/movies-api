99f88ab951b7d6e70e1f8f131dd945cc7d7af29e
Lê Minh Bảo <besilent308@gmail.com>
2024-02-21 09:27:24 +0700

    Setup CORS configuration

---
+++ src/main/java/com/leminhbao/moviesapi/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.leminhbao.moviesapi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");  // Chỉ cho phép tất cả các origin, bạn có thể thay đổi theo nhu cầu
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
