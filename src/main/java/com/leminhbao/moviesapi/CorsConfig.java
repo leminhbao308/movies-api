2d9932527ebbf59192b3ea3ed2c7ac97270c6b94
Lê Minh Bảo <besilent308@gmail.com>
2024-02-21 09:48:07 +0700

    Add Bean annotation to CORS configuration

--- src/main/java/com/leminhbao/moviesapi/CorsConfig.java
+++ src/main/java/com/leminhbao/moviesapi/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.leminhbao.moviesapi;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -8,6 +9,7 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
+    @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
